ae67b400b917add4db4d095a65f9a24313540ff6
Raul Vitor <rvt.gc13620@gmail.com>
2026-07-03 20:01:13 -0300

    Add files via upload

---
+++ AcademiaApp.cs
@@ -0,0 +1,34 @@
+﻿List<Exercicio> ListaDeExercicio = new();
+string continuar = "s";
+
+while (continuar.ToLower() == "s") 
+{
+    Console.WriteLine("----vamos cadastrar o treino!----");
+
+    Console.Write("digite nome do exercicio: ");
+    string Nome = Console.ReadLine();
+
+    Console.Write("digite quantas serie: ");
+    int Serie = int.Parse(Console.ReadLine());
+
+    Console.Write("digite quantas repeticao: ");
+    int Repeticao = int.Parse(Console.ReadLine());
+
+    Console.Write("digite quantas carga: ");
+    int Carga = int.Parse(Console.ReadLine());
+
+    Exercicio novoExercicio = new Exercicio(Nome, Serie, Repeticao, Carga);
+    ListaDeExercicio.Add(novoExercicio);
+
+    Console.Write("\nDeseja cadastrar mais um exercício? (s/n): ");
+    continuar = Console.ReadLine();
+}
+
+    Console.WriteLine("----Treino Completo----");
+
+    foreach (Exercicio cada in ListaDeExercicio) 
+    {
+    cada.IniciarTreino();
+    }
+   
+
---
+++ Exercicio.cs
@@ -0,0 +1,35 @@
+﻿class Exercicio {
+
+    public string Nome {  get; set; }
+    public int Serie { get; set; }
+    public int Repeticao { get; set; }
+    public int Carga {  get; set; }
+
+    public Exercicio(string Nome, int serie, int Repeticao, int Carga) {
+
+        this.Nome = Nome;
+        this.Serie = serie;
+        this.Repeticao = Repeticao;
+        this.Carga = Carga;
+
+    }
+
+    public void IniciarTreino() {
+
+        {
+
+            if (Carga <= 0) {
+                Console.WriteLine($"Exercicio: {Nome} a serie é {Serie} de {Repeticao}, com essa carga so da barra!");
+            }
+            else if (Carga >= 100) {
+                Console.WriteLine($"Exercicio: {Nome}, com essa carga {Carga}, vamos diminuir um pouco as series e repetições! ");
+            }
+            else {
+                Console.WriteLine($"Exercicio: {Nome} a serie é {Serie} de {Repeticao}, com essa carga {Carga}");
+            }
+        }
+    }
+
+}
+
+
